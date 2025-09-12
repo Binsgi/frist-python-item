a2c95aa89d90e8ded316f8704e82bd56fb98722f
BingShan <3464411620@qq.com>
2025-09-12 13:30:30 +0800

    修改了bug

--- game_functions.py
+++ game_functions.py
@@ -12,22 +12,22 @@ def check_keydown_events(event,ship):
 
 def check_keyup_events(event,ship):
     """响应松开"""
-    if event.type == pygame.K_RIGHT:
+    if event.key == pygame.K_RIGHT:
         ship.moving_right = False
-    elif event.type == pygame.K_LEFT:
+    elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
 def check_events(ship):
-    #监视键盘鼠标事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                check_keydown_events(event,ship)
-            elif event.type == pygame.KEYUP:
-                check_keyup_events(event,ship)
+    """响应按键和鼠标事件"""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ship)
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
 
-def update_screen(ai_settings,screen,ship):
+def update_screen(ai_settings, screen, ship):
     """更新屏幕上的图像，并切换到新屏幕"""
      #每次循环时都重绘屏幕
     screen.fill(ai_settings.bg_color)
