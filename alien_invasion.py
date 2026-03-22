84471571e0ac40c47fa599d14e784f21a31e7733
BingShan <3464411620@qq.com>
2026-03-22 16:01:45 +0800

    基于python飞机大战怪兽小游戏项目

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from pygame.sprite import Group
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
 from button import Button
 from ship import Ship
 from alien import Alien
@@ -18,8 +19,9 @@ def run_game():
     #创建Play按钮
     play_button = Button(ai_settings, screen, "Play")
 
-    #创建一个用于存储游戏统计信息的实例
+    #创建一个用于存储游戏统计信息的实例,并创建记分牌
     stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
 
     #创建一支飞船，一个子弹编组，一个外星人编组
     ship = Ship(ai_settings,screen)
@@ -31,15 +33,18 @@ def run_game():
 
     #开始游戏的主循环
     while True:
-        gf.check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
+        gf.check_events(ai_settings, screen, stats, sb, play_button, ship,
+             aliens, bullets)
         
         if stats.game_active:
             ship.update()
-            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
-            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+            gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens,
+                bullets)
+            gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens,
+                bullets)
 
-        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets,
-            play_button)
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens,
+            bullets, play_button)
 
         #让最近绘制的屏幕可见
         pygame.display.flip()
